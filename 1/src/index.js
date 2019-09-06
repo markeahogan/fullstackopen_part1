b69809061601d7cd67bda512234c87c7a296e1ef
Mark Hogan <markeahogan@gmail.com>
2019-09-06 18:37:21 +0000

    Exercise 1.1 course information, step 1

--- 1/src/index.js
+++ 1/src/index.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const Hello = (props) => (
-    <div>
-        <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
+const Header = (props) => (
+    <h1>{props.course}</h1>
 )
 
-const Footer = () => {
-    return (
-        <div>
-            greeting app created by
-            <a href="https://github.com/mluukkai">mluukkai</a>
-        </div>
-    )
+const Content = (props) => {
+    return props.parts.map(part => <p>{part[0]} {part[1]}</p>);
 }
 
-const App = () => (
-    <>
-        <h1>Greetings</h1>
-        <Hello name="Harry" age={6}/>
-        <Footer />
-    </>
+const Total = (props) => (
+    <p>Number of exercises {props.count}</p>
 )
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={[[part1, exercises1], [part2, exercises2], [part3, exercises3]]} />
+      <Total count={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))

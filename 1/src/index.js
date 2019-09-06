3c1ae8f888cf5fd2deced7003978d6628029961e
Mark Hogan <markeahogan@gmail.com>
2019-09-06 08:49:56 +0000

    Before excercise 1

--- 1/src/index.js
+++ 1/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (
+const Hello = (props) => (
     <div>
-        <p>Hello World</p>
+        <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
+)
+
+const Footer = () => {
+    return (
+        <div>
+            greeting app created by
+            <a href="https://github.com/mluukkai">mluukkai</a>
+        </div>
+    )
+}
+
+const App = () => (
+    <>
+        <h1>Greetings</h1>
+        <Hello name="Harry" age={6}/>
+        <Footer />
+    </>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'));

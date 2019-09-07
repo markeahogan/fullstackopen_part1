ceb3783a74661d327349731cc72ffeb72e99a93f
Mark Hogan <markeahogan@gmail.com>
2019-09-07 19:27:21 +0000

    1.6 unicafe step1

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Button = ({onClick, text}) => (
+    <button onClick={onClick}>{text}</button>
+)
+
+const Statistics = ({good, neutral, bad}) => (
+    <>
+    <div>good {good}</div>
+    <div>neutral {neutral}</div>
+    <div>bad {bad}</div>
+    </>
+)
+
 const App = () => {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
 
     return (
-        <div>
-            code here
-        </div>
+        <>
+            <h1>give feedback</h1>
+            <Button text="good" onClick={() => setGood(good+1)}/>
+            <Button text="neutral" onClick={() => setNeutral(neutral+1)}/>
+            <Button text="bad" onClick={() => setBad(bad+1)}/>
+            <h1>statistics</h1>
+            <Statistics good={good} neutral={neutral} bad={bad}/>
+        </>
     )
 }
 
